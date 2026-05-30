c6768b1df997a608a3ae323157e0125a12bf471f
Lolipop <makeanonim@gmail.com>
2026-05-30 18:25:44 +0000

    Revert "include/linux: Import Clover changes"
    This reverts commit 127066518eb801affda96435e3fe853fd0f269e7.

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -537,7 +537,7 @@ struct fb_info {
 	} *apertures;
 
 	bool skip_vt_switch; /* no VT switch on suspend/resume required */
-#if defined(CONFIG_MACH_XIAOMI_PLATINA) || defined(CONFIG_MACH_XIAOMI_CLOVER)
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
 	int blank;
 #endif
 };
