65bae142e5a8b981be6d95894b73e81a572996d5
Lolipop <makeanonim@gmail.com>
2026-06-05 07:12:14 +0000

    Revert "‎drivers/power/supply/qcom/qpnp-smb2.c‎: refactor charging property handling for Xiaomi platforms to fix battery health %"
    This reverts commit 96effc87fc6cbe2140f9915015aafd9ba6e2af85.

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1247,7 +1247,10 @@ static int smb2_init_dc_psy(struct smb2 *chip)
  *************************/
 
 static enum power_supply_property smb2_batt_props[] = {
-	POWER_SUPPLY_PROP_INPUT_SUSPEND,
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
+	POWER_SUPPLY_PROP_CHARGING_ENABLED,
+#endif
+    POWER_SUPPLY_PROP_INPUT_SUSPEND,
 	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_HEALTH,
 	POWER_SUPPLY_PROP_PRESENT,
@@ -1307,17 +1310,11 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_PRESENT:
 		rc = smblib_get_prop_batt_present(chg, val);
 		break;
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
 	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
-#ifdef CONFIG_MACH_XIAOMI_CLOVER
-		val->intval = get_effective_result(chg->usb_icl_votable);
-		if (val->intval < 0) /* no votes */
-			val->intval = 1;
-		else
-			val->intval = !!val->intval;
-#else
 		val->intval = !get_effective_result(chg->chg_disable_votable);
-#endif
 		break;
+#endif
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 		rc = smblib_get_prop_input_suspend(chg, val);
 		break;
@@ -1413,7 +1410,7 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
 	case POWER_SUPPLY_PROP_CHARGE_FULL:
 #ifndef CONFIG_MACH_XIAOMI_PLATINA
-	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:	
+	case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
 #endif
 	case POWER_SUPPLY_PROP_CYCLE_COUNT:
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
@@ -1461,13 +1458,11 @@ static int smb2_batt_set_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_STATUS:
 		rc = smblib_set_prop_batt_status(chg, val);
 		break;
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
 	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
-#ifdef CONFIG_MACH_XIAOMI_CLOVER
-		rc = vote(chg->usb_icl_votable,DEFAULT_VOTER, !val->intval, 0);
-#else
 		vote(chg->chg_disable_votable, USER_VOTER, !!!val->intval, 0);
-#endif
 		break;
+#endif
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 		rc = smblib_set_prop_input_suspend(chg, val);
 		break;
@@ -1556,7 +1551,9 @@ static int smb2_batt_prop_is_writeable(struct power_supply *psy,
 {
 	switch (psp) {
 	case POWER_SUPPLY_PROP_STATUS:
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
 	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+#endif
 	case POWER_SUPPLY_PROP_INPUT_SUSPEND:
 	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
 	case POWER_SUPPLY_PROP_CAPACITY:
